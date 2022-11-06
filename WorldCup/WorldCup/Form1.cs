09c364e560f7e40ae6127deb2f6a2dbe9d83a8d8
Imanda03 <asis03ktm@gmail.com>
2022-11-06 17:26:44 +0545

    database connection

---
+++ WorldCup/WorldCup/Form1.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace WorldCup
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            List<int> jersyNo = new List<int>();
+            for(int i=1;i<=100; i++)
+            {
+                jersyNo.Add(i);
+            }
+            comboBox2.DataSource = jersyNo;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn = new SqlConnection("Data Sorce=.\\sqlexpress;Initial Catalog=WorldCup; Integrated Security=True");
+            SqlCommand cmd = new SqlCommand("insert into Player values('Anish', 'GoalKeeper', 4, 1)",conn);
+        try{
+                cmd.Connection.Open();
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("SuccessFully saved");
+        }
+            catch(Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                cmd.Connection.Close();
+            }
+        }
+    }
+}
