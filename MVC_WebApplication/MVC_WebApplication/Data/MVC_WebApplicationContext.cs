81c6b96995c0f236a9274808a48f8b728b59e343
Imanda03 <asis03ktm@gmail.com>
2022-11-27 21:07:14 +0545

    .

---
+++ MVC_WebApplication/MVC_WebApplication/Data/MVC_WebApplicationContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MVC_WebApplication;
+
+namespace MVC_WebApplication.Data
+{
+    public class MVC_WebApplicationContext : DbContext
+    {
+        public MVC_WebApplicationContext (DbContextOptions<MVC_WebApplicationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MVC_WebApplication.Players> Players { get; set; } = default!;
+    }
+}
---
+++ MVC_WebApplication/MVC_WebApplication/Players.cs
@@ -0,0 +1,14 @@
+﻿namespace MVC_WebApplication
+{
+    public class Players
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+
+        public string Position { get; set; }
+
+        public int JNo { get; set; }
+
+        public int CId { get; set; }
+    }
+}
