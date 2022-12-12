212bff184110764b85dde78585cf51f3c5f7fcb3
Imanda03 <asis03ktm@gmail.com>
2022-12-12 21:52:03 +0545

    .

--- 2022/WebApplication1/WebApplication1/Models/Product.cs
+++ 2022/WebApplication1/WebApplication1/Models/Product.cs
@@ -1,12 +1,17 @@
 ﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace WebApplication1.Models
 {
     public class Product
     {
         public int Id { get; set; }
+        [Required(ErrorMessage ="Title is required")]
+        [StringLength(maximumLength:20, ErrorMessage ="Character are crossing their limits")]
+        [RegularExpression("^[a-zA-Z]")]
         public string Title { get; set; }
 
+        [DataType(DataType.Currency)]
         public int Price { get; set; }
 
         public string Description { get; set; }
